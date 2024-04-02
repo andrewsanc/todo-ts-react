81f0c4ac634eab72919ed41389f184f49771ddb3
TB831 <druu831@gmail.com>
2024-04-02 16:11:17 -0700

    feat. change hero image to appropiate dark hero img

--- src/components/ui/hero-image.tsx
+++ src/components/ui/hero-image.tsx
@@ -1,9 +1,16 @@
 import BgLight from "../../assets/bg-desktop-light.jpg";
+import BgDark from "../../assets/bg-desktop-dark.jpg";
+import { useTheme } from "../../lib/hooks";
 
 export default function HeroImage() {
+  const { theme } = useTheme();
+
   return (
     <div className="absolute w-full h-[200px] sm:h-[300px] bg-center bg-no-repeat">
-      <img className="w-full h-full object-cover" src={BgLight} />
+      <img
+        className="w-full h-full object-cover"
+        src={theme === "light" ? BgLight : BgDark}
+      />
     </div>
   );
 }
